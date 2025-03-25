0df5aba6b1950447163ed61332af1cde87eeb86f
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-03-25 16:23:52 +0530

    completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
       <footer className="bg-gray-900 text-white py-6 px-4">
         <div className="max-w-6xl mx-auto text-center">
           <p className="text-sm">
-            © 2021 ABAX ACADEMY. All Rights Reserved | Design by Iceico Technologies
+            © 2025 ABAX ACADEMY. All Rights Reserved | Design by Dhananjay Sable ICEICO Technologies
           </p>
         </div>
       </footer>
