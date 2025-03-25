f69f299d98267c0e4b9f24556156a77b48e08431
Dhananjay Sable <dhananjaysable370@gmail.com>
2025-03-25 16:43:31 +0530

    completed

--- src/components/DirectionsRegistration.jsx
+++ src/components/DirectionsRegistration.jsx
@@ -39,7 +39,7 @@ const DirectionsRegistration = () => {
           </div>
 
           <div className="md:w-1/2 md:pl-8">
-            <h3 className="text-3xl md:text-4xl font-semibold mb-6">
+            <h3 className="text-3xl md:text-4xl font-bold mb-6">
               Registration <span className="font-normal">Form</span>
             </h3>
 
